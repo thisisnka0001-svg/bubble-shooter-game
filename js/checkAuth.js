ed5492847adba2f4f8473ee3419c58d32e29bbb5
thisisnka0001-svg <thisisnka0001@gmail.com>
2026-07-02 06:56:30 +0530

    Create checkAuth.js

---
+++ js/checkAuth.js
@@ -0,0 +1,12 @@
+import { auth } from "./firebase.js";
+import { onAuthStateChanged } from "https://www.gstatic.com/firebasejs/12.5.0/firebase-auth.js";
+
+onAuthStateChanged(auth, (user) => {
+
+    if(!user){
+
+        window.location.href="login.html";
+
+    }
+
+});

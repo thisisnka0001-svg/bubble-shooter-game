9c7f01c410ecf38c69f2b2aaa465bce22e06f939
thisisnka0001-svg <thisisnka0001@gmail.com>
2026-07-02 06:00:23 +0530

    Update auth.js

--- js/auth.js
+++ js/auth.js
@@ -1,3 +1,28 @@
-import { auth, db } from "./firebase.js";
+import { auth } from "./firebase.js";
+import { createUserWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/12.5.0/firebase-auth.js";
 
-console.log("✅ Firebase Connected Successfully");
+const registerBtn = document.getElementById("registerBtn");
+
+registerBtn.addEventListener("click", async () => {
+
+    const email = document.getElementById("email").value;
+    const password = document.getElementById("password").value;
+
+    if(email === "" || password === ""){
+        alert("Please fill all fields");
+        return;
+    }
+
+    try{
+
+        await createUserWithEmailAndPassword(auth,email,password);
+
+        alert("Account Created Successfully 🎉");
+
+    }catch(error){
+
+        alert(error.message);
+
+    }
+
+});

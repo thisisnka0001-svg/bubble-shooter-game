3c9dbbf2e71a833486fcc97d98a0771848c9e30d
thisisnka0001-svg <thisisnka0001@gmail.com>
2026-07-02 05:43:27 +0530

    Update firebase.js

--- js/firebase.js
+++ js/firebase.js
@@ -1,1 +1,17 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.5.0/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/12.5.0/firebase-auth.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/12.5.0/firebase-firestore.js";
 
+const firebaseConfig = {
+  apiKey: "AIzaSyDBx_OXRa2TUussw5FRi5zp8Ttofvmh17A",
+  authDomain: "bubblerewards.firebaseapp.com",
+  projectId: "bubblerewards",
+  storageBucket: "bubblerewards.firebasestorage.app",
+  messagingSenderId: "641168553021",
+  appId: "1:641168553021:web:d6f3533da37718277b2654"
+};
+
+const app = initializeApp(firebaseConfig);
+
+export const auth = getAuth(app);
+export const db = getFirestore(app);
